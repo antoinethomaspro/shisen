306b996d8806326b5ce9a51c47e42980599f43bb
Antoine <antoine.thomas654@gmail.com>
2018-12-23 18:22:17 +0100

    Added Test Function

---
+++ src/ShisenGame.java
@@ -0,0 +1,21 @@
+
+public class ShisenGame {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+Board jeu = new Board();
+jeu.init();
+//jeu.shuffleArray();
+Renderer render = new Renderer();
+render.draw(jeu);
+
+
+
+
+
+	}
+
+}
